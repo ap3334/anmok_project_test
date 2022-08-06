1a5553b8e55cec6c6d940f9fae0789d013858273
ap3334 <chwil2989@naver.com>
2022-08-06 20:27:12 +0900

    homepage -> home 수정

--- your-anmok/src/main/java/com/lion/youranmok/home/controller/HomePageController.java
+++ your-anmok/src/main/java/com/lion/youranmok/home/controller/HomePageController.java
@@ -1,17 +1,17 @@
-package com.lion.youranmok.homepage.homepage;
+package com.lion.youranmok.home.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/homepage")
+@RequestMapping("/home")
 public class HomePageController {
 
-    @GetMapping("/homepage")
-    public String homepage() {
+    @GetMapping()
+    public String home() {
 
-        return "homepage/main";
+        return "home";
 
     }
 
